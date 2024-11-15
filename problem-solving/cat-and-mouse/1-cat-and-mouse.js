4de43adba34d60c02bc0d34fc23be1dc3f17a290
Svitlana Horodylova <horodylova.sv@gmail.com>
2024-11-15 16:40:25 +0000

    1-cat

--- problem-solving/cat-and-mouse/1-cat-and-mouse.js
+++ problem-solving/cat-and-mouse/1-cat-and-mouse.js
@@ -1,6 +1,32 @@
 const { check, runTest, skipTest } = require("../../test-api/index.js");
 
 function calculateJump(locations, jumpLength) {
+ 
+  const catIndex = locations.indexOf("cat");
+  const mouseIndex = locations.indexOf("mouse");
+
+  
+  if (catIndex === -1 || mouseIndex === -1) {
+    throw new Error("The array must contain both 'cat' and 'mouse'.");
+  }
+
+  
+  const start = Math.min(catIndex, mouseIndex) + 1;
+  const end = Math.max(catIndex, mouseIndex);
+
+  
+  let counter = 0;
+  for (let i = start; i < end; i++) {
+    if (locations[i] === "W") {
+      counter += 2; 
+    } else if (locations[i] === "x") {
+      counter += 1; 
+    }
+  }
+
+  
+  return counter < jumpLength;
+}
 /*
 You will be given an array containing string representations of the locations of a cat and a mouse. The array may also contain walls represented by "W".
 You will also be given a positive integer which represents how far the cat can jump.
@@ -10,7 +36,6 @@ Your task is to calculate if the cat can jump far enough to catch the mouse.
 Each space "x" expends 1 of the cat's movement, and each wall "W" expends 2. 
 It does not matter if the cat is before or after the mouse in the array.
 */
-}
 
 runTest(
   "cat catches the mouse when it's closer than the jump length",
@@ -21,7 +46,7 @@ runTest(
   }
 );
 
-skipTest(
+runTest(
   "cat fails to catch the mouse when jump length is smaller than distance",
   function () {
     check(calculateJump(["cat", "W", "mouse"], 2)).isEqualTo(false);
